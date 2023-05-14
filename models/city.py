ae64d07a6e121e891454e4d4994bfa48a120a098
Hafsa Hussien <117745174+hedishafi@users.noreply.github.com>
2023-05-14 19:37:49 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -16,7 +16,7 @@ class City(BaseModel):
     This is the City Class ...
 
     Attributes:
-        state_id (str): The UUID of the State the City belongs t
+        state_id (str): The UUID of the State the City belongs to
         name (str): The City name
 
     """
