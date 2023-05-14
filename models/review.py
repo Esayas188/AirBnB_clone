491ffd5312aae0793f97e8f697d4d9f7941d9400
Hafsa Hussien <117745174+hedishafi@users.noreply.github.com>
2023-05-14 19:41:34 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -16,7 +16,7 @@ class Review(BaseModel):
     Attributes:
         place_id (str): The UUID of the Place the Review belongs to
         user_id (str): The UUID of the User that made the review
-        text (str): The message the User wrote about the Plac
+        text (str): The message the User wrote about the Place
 
     """
     place_id = ''
